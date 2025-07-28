51c81856c660c7b112b40edd63c86036461b276a
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-07-28 19:42:03 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <sstream>
+#include <math.h>
+#include <time.h>
+using namespace std;
+static long long int N, A;
+static vector<vector<int>> v;
+static vector<bool> visited;
+map<int, pair<int, int>> m;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    string fan = ":fan:";
+    string in;
+    cin >> in;
+    cout << fan << fan << fan << "\n";
+    cout << fan << ":" << in << ":" << fan << "\n";
+    cout << fan << fan << fan;
+}
