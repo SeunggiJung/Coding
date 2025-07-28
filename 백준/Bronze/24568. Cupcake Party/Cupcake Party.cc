6f93ac2128b8a126e78cc74b2d17450a4bcabb85
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-07-28 19:51:27 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+static long long int sum;
+static long long int N, M;
+struct compare {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+};
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> N >> M;
+    cout << N * 8 + M * 3 - 28;
+}
