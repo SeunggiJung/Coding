9f8e6a4d0d3006d7af8aaf27ee9aab88ac113ab3
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-07-28 19:03:16 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <sstream>
+#include <math.h>
+#include <time.h>
+#include <list>
+using namespace std;
+static long long int N, M, X, Y;
+static vector<vector<int>> v;
+static vector<bool> visited;
+static vector<pair<int, int>> direction;
+static int cnt;
+map<int,vector<pair<int,int>>> m;
+static int wei;
+struct compare {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    string in;
+    cin >> in;
+    
+    for (int i = 0; i < in.length(); i++) {
+        if (isupper(in[i])) {
+            in[i]=tolower(in[i]);
+        }
+        else {
+            in[i]=toupper(in[i]);
+        }
+    }
+    
+    cout << in;
+}
