bfb9cd350cf65fac543c6dfed24b5e79ebdea619
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-07-28 19:30:20 +0900

    [Gold III] Title: 줄 세우기, Time: 1652 ms, Memory: 5840 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+static long long int sum;
+static long long int N, M;
+static int mi, zero, pl;
+struct compare {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin >> N >> M;
+    vector<int> v(N + 1, 0);
+    map < int, vector<int>> m;
+    for (int i = 1; i <= N; i++) {
+        vector<int> x;
+        m.insert({ i,x });
+    }
+    queue<int> q;
+    for (int i = 0; i < M; i++) {
+        int x, y;
+        cin >> x >> y;
+        v[y]++;
+        m[x].push_back(y);
+    }
+    int cnt = 0;
+    while (true) {
+        for (int i = 1; i <= N; i++) {
+            if (v[i] == 0) {
+                int len = m[i].size();
+                q.push(i);
+                cnt++;
+                v[i] = -1;
+                for (int j = 0; j < len; j++) {
+                    v[m[i][j]] -= 1;
+                }
+            }
+        }
+        if (cnt==N) break;
+    }
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+}
