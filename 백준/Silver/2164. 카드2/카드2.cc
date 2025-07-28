1dd6ebc5ad3ae00d3ae2d567d4599d94e685b886
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-07-28 19:29:44 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	queue<int> cards;
+	for (int i = 1; i <= N; i++) {
+		cards.push(i);
+	}
+	while (cards.size()!=1) {
+		cards.pop();
+		cards.push(cards.front());
+		cards.pop();
+	}
+	cout << cards.front();
+}
