716d15a61db4ebdef83f140275737c8d370ccf91
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-07-28 19:37:25 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+static long long int sum;
+static long long int N, M;
+struct compare {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+};
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|";
+}
