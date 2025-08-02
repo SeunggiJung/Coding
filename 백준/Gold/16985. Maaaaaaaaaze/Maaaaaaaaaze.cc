843feb0176d73a8a6e85ea57952f92a971bc8f54
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-08-02 15:28:44 +0900

    [Gold II] Title: Maaaaaaaaaze, Time: 1052 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.cc
+++ 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.cc
@@ -1,85 +1,90 @@
 #include <iostream>
+
 #include <vector>
+
 #include <algorithm>
+
 #include <numeric>
+
 #include <queue>
+
 using namespace std;
+
 typedef pair<int, int> pii;
-vector<vector<vector<int>>> origin_board(5, vector<vector<int>>(5, vector<int>(5)));
+
+vector<vector<vector<int>>> board(5, vector<vector<int>>(5, vector<int>(5)));
 int dx[] = { 1,-1,0,0,0,0 };
 int dy[] = { 0,0,1,-1,0,0 };
 int dz[] = { 0,0,0,0,1,-1 };
-int ans = 126;
-vector<int> layer_dir(5);
-vector<int> order = { 0,1,2,3,4 };
-vector<vector<int>> rotate(vector<vector<int>> &piece) {
-    vector<vector<int>> rotated(5, vector<int>(5));
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            rotated[j][4 - i] = piece[i][j];
-        }
-    }
-    return rotated;
-}
-void bfs(const vector<vector<vector<int>>> &board) {
-    vector<vector<vector<int>>> visited(5, vector<vector<int>>(5, vector<int>(5, 0)));
-    queue<vector<int>> q; // x,y,z
-    q.push({ 0,0,0 });
-    while (!q.empty()) {
-        int x = q.front()[0];
-        int y = q.front()[1];
-        int z = q.front()[2];
-        q.pop();
-        if (x == 4 && y == 4 && z == 4) {
-            ans = min(ans, visited[x][y][z]);
-            break;
-        }
-        for (int i = 0; i < 6; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            int nz = z + dz[i];
-            if (nx < 0 || ny < 0 || nz < 0 || nx >= 5 || ny >= 5 || nz >= 5) continue;
-            if (visited[nx][ny][nz]) continue;
-            if (board[nx][ny][nz] == 0) continue;
-            visited[nx][ny][nz] = visited[x][y][z] + 1;
-            q.push({ nx,ny,nz });
-        }
-    }
-}
-void permu(int layer) {
-    if (layer==5) {
-        vector<vector<vector<int>>> board(5, vector<vector<int>>(5, vector<int>(5)));
-        for (int i = 0; i < 5; i++) {
-            vector<vector<int>> temp = origin_board[order[i]];
-            for (int j = 0; j < layer_dir[i]; j++) {
-                temp = rotate(temp);
-            }
-            board[i] = temp;
-        }
-        if (!board[0][0][0] || !board[4][4][4]) return;
-        bfs(board);
-        return;
-    }
-    for (int i = 0; i < 4; i++) {
-        layer_dir[layer] = i;
-        permu(layer + 1);
-    }
+void rotate(int layer) {
+	vector<vector<int>> rotated(5, vector<int>(5));
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			rotated[j][4 - i] = board[layer][i][j];
+		}
+	}
+	board[layer] = rotated;
 }
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < 5; k++) {
-                cin >> origin_board[i][j][k];
-            }
-        }
-    }
-    do {
-        permu(0);
-    } while (next_permutation(order.begin(), order.end()));
-
-    if (ans == 126) cout << -1;
-    else cout << ans;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			for (int k = 0; k < 5; k++) {
+				cin >> board[i][j][k];
+			}
+		}
+	}
+	//4^5 = 1024
+	int ans = 126;
+	for (int a = 0; a < 4; a++) {
+		rotate(0);
+		for (int b = 0; b < 4; b++) {
+			rotate(1);
+			for (int c = 0; c < 4; c++) {
+				rotate(2);
+				for (int d = 0; d < 4; d++) {
+					rotate(3);
+					for (int e = 0; e < 4; e++) {
+						rotate(4);
+						vector<int> permutationIndices(5); // 순열 생성을 위한 인덱스
+						iota(permutationIndices.begin(), permutationIndices.end(), 0); // 0, 1, 2, 3, 4로 초기화
+						do {
+							vector<vector<vector<int>>> current_board;
+							for (int index : permutationIndices) {
+								current_board.push_back(board[index]);
+							}
+							if (!current_board[0][0][0] || !current_board[4][4][4]) continue;
+							vector<vector<vector<int>>> visited(5, vector<vector<int>>(5, vector<int>(5, 0)));
+							queue<vector<int>> q; // x,y,z
+							q.push({ 0,0,0 });
+							while (!q.empty()) {
+								int x = q.front()[0];
+								int y = q.front()[1];
+								int z = q.front()[2];
+								q.pop();
+								if (x == 4 && y == 4 && z == 4) {
+									ans = min(ans, visited[x][y][z]);
+									break;
+								}
+								for (int i = 0; i < 6; i++) {
+									int nx = x + dx[i];
+									int ny = y + dy[i];
+									int nz = z + dz[i];
+									if (nx < 0 || ny < 0 || nz < 0 || nx >= 5 || ny >= 5 || nz >= 5) continue;
+									if (visited[nx][ny][nz]) continue;
+									if (current_board[nx][ny][nz] == 0) continue;
+									visited[nx][ny][nz] = visited[x][y][z] + 1;
+									q.push({ nx,ny,nz });
+								}
+							}
+						} while (next_permutation(permutationIndices.begin(), permutationIndices.end()));
+					}
+				}
+			}
+		}
+	}
+	if (ans == 126) cout << -1;
+	else cout << ans;
 }
