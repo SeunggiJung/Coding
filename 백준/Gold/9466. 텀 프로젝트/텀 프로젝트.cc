525ce91c93e51ad802a8bead177e3934b59d54e2
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-11-03 21:40:39 +0900

    [Gold III] Title: 텀 프로젝트, Time: 456 ms, Memory: 3984 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+typedef pair<int,int> pii;
+vector<int> v;
+vector<int> visited;
+int T,N,ans;
+void dfs(int start, int cur){
+    visited[cur] = start;
+    if(visited[v[cur]] == start){
+        int cnt=0;
+        int X = v[cur];
+        while(true){
+            cnt++;
+            visited[X] = true;
+            X = v[X];
+            if(X==v[cur]) break;
+        }
+        ans-=cnt;
+        return;
+    }
+    if(visited[v[cur]]) return;
+    dfs(start, v[cur]);
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin>>T;
+    while(T-->0){
+        cin>>N;
+        v.resize(N+1);
+        visited.assign(N+1, false);
+        ans = N;
+        for(int i=1;i<=N;i++){
+            cin>>v[i];
+        }
+        for(int i=1;i<=N;i++){
+            if(visited[i]) continue;
+            dfs(i, i);
+        }
+        cout<<ans<<"\n";
+    }
+
+}
