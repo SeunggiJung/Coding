7780acecd7c191ff0c20f72bec8935f288df9fca
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-11-06 17:35:23 +0900

    [Gold IV] Title: 루머, Time: 560 ms, Memory: 34760 KB -BaekjoonHub

---
+++ 백준/Gold/19538. 루머/루머.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <map>
+using namespace std;
+typedef pair<int, int> pii;
+int N, M;
+vector<int> visited;
+vector<int> trust;
+map<int, vector<int>> m;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>N;
+    visited.resize(N+1, 0);
+    trust.resize(N+1, -1);
+    for(int i=1;i<=N;i++){
+        int to;
+        while(true){
+            cin>>to;
+            if(!to) break;
+            m[i].push_back(to);
+        }
+    }
+    cin>>M;
+    queue<pii> q;
+    for(int i=0;i<M;i++){
+        int x;
+        cin>>x;
+        trust[x] = 0;
+        q.push({x, 0});
+    }
+    while(!q.empty()){
+        int cur = q.front().first;
+        int time = q.front().second;
+        q.pop();
+        for(int next : m[cur]){
+            if(trust[next] != -1) continue;
+            visited[next]++;
+            if(visited[next] >= (m[next].size() + 1)/2){
+                //cout<<cur<<"->"<<next<<" "<<visited[next]<<" "<<m[next].size()<<"!\n";
+                trust[next] = time+1;
+                q.push({next, time+1});
+            }
+            //q.push({next, time + 1});
+        }
+    }
+    for(int i=1;i<=N;i++){
+        cout<<trust[i]<<" ";
+    }
+}
