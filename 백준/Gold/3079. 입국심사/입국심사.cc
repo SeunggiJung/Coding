cf71d8d379d2aadd5d0ab380633eef520e6ad057
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-11-22 22:22:52 +0900

    [Gold V] Title: 입국심사, Time: 44 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.cc
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -1,42 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <queue>
-#include <string>
-#include <sstream>
-#include <map>
-#include <math.h>
+#include <vector>
 using namespace std;
-typedef unsigned long long int lli;
-typedef pair<lli, lli> pll;
-static int N;
-static lli M;
-struct compare {
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
-        return a.second > b.second;
-    }
-};
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    cin >> N >> M;
-    vector<int> v(N);
-    for (int i = 0; i < N; i++) {
-        cin >> v[i];
-    }
-    sort(v.begin(), v.end());
-    lli start = v.front(), end = v.front() * M;
-    while (start <= end) {
-        lli mid = (start + end) / 2;
-        lli cnt = 0;
-        for (int i = 0; i < N; i++) cnt += (mid / v[i]);
-        if (cnt >= M) {
-            end = mid-1;
-        }
-        else {
-            start = mid+1;
-        }
-    }
-    cout << start;
+
+long long N, M;
+vector<long long> test;
+long long Test(long long left, long long right, long long rst)
+{
+	if (left > right) return rst;
+	long long mid = (left + right) / 2;
+
+	long long sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		sum += (mid / test[i]);
+		if (sum >= M) return Test(left, mid - 1, mid);
+	}
+	return Test(mid + 1, right, rst);
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> N >> M;
+	test.resize(N);
+	long long Max = 0;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> test[i];
+		Max = max(Max, test[i]);
+	}
+	long long end = Max * M;
+	cout << Test(1, end, end) << '\n';
 }
