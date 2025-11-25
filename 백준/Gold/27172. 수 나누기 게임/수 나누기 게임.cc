4df76a356e8fd89d5f07b44f0f992f9044ee6444
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-11-25 21:17:59 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 48 ms, Memory: 6712 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -1,35 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <cmath>
 #include <map>
 using namespace std;
-typedef pair<int,int> p;
 int N;
 vector<int> v;
-map<int, int> m;
-vector<vector<bool>> es;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     cin>>N;
     int maxi=0;
-    vector<int> nums(N);
-    v.assign(1000001, false);
+    vector<int> nums(N, 0);
+    v.assign(1000001, -1);
     for(int i=0;i<N;i++){
         cin>>nums[i];
         maxi = max(maxi,nums[i]);
-        m[nums[i]] = i;
-        v[nums[i]] = true;
+        v[nums[i]] = i;
     }
     vector<int> score(N, 0);
     for(int i=0;i<N;i++){
         for(int j=nums[i]*2;j<=maxi;j+=nums[i]){
-            if(v[j]){
+            if(v[j]>=0){
                 score[i]++;
-                score[m[j]]--;
+                score[v[j]]--;
             }
         }
     }
