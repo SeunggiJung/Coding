f3a097a1f78667e81a04ac1836b2c15d50becb92
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2025-12-20 19:39:16 +0900

    [Gold III] Title: 줄 세우기, Time: 52 ms, Memory: 5704 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,53 +1,36 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <map>
 #include <algorithm>
-#include <sstream>
+#include <map>
 using namespace std;
-static long long int sum;
-static long long int N, M;
-static int mi, zero, pl;
-struct compare {
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
-        return a.second > b.second;
-    }
-};
-
+typedef pair<int,int> pii;
+int N, M;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    cin >> N >> M;
-    vector<int> v(N + 1, 0);
-    map < int, vector<int>> m;
-    for (int i = 1; i <= N; i++) {
-        vector<int> x;
-        m.insert({ i,x });
+    cin>>N>>M;
+    vector<int> cnt(N+1, 0);
+    map<int, vector<int>> st; // smaller than
+    for(int i=0;i<M;i++){
+        int A, B;
+        cin>>A>>B;
+        cnt[B]++;
+        st[A].push_back(B);
     }
-    queue<int> q;
-    for (int i = 0; i < M; i++) {
-        int x, y;
-        cin >> x >> y;
-        v[y]++;
-        m[x].push_back(y);
+    queue<int> q; // first : who, second : cur_cnt
+    for(int i=1;i<=N;i++){
+        if(!cnt[i]) q.push(i);
     }
-    int cnt = 0;
-    while (true) {
-        for (int i = 1; i <= N; i++) {
-            if (v[i] == 0) {
-                int len = m[i].size();
-                q.push(i);
-                cnt++;
-                v[i] = -1;
-                for (int j = 0; j < len; j++) {
-                    v[m[i][j]] -= 1;
-                }
-            }
-        }
-        if (cnt==N) break;
-    }
-    while (!q.empty()) {
-        cout << q.front() << " ";
+    while(!q.empty()){
+        int n = q.front();
         q.pop();
+        cout<<n<<" ";
+        for(int next : st[n]){
+            if(cnt[next]<1) continue;
+            cnt[next]--;
+            if(!cnt[next]) q.push(next);
+        }
     }
 }
