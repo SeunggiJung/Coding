b029092c458c0b3c589de3a5a2c6dbbf78058725
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2026-02-22 20:34:08 +0900

    [Gold III] Title: 웜홀, Time: 440 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -29,9 +29,7 @@ int main() {
         }
         for(int k=1;k<=N;k++){
             for(int i=1;i<=N;i++){
-                if(i==k) continue;
                 for(int j=1;j<=N;j++){
-                    if(k==j) continue;
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
