f693b085fb1cfcb4a88de55a6f206490932aeea6
SeunggiJung <122208238+SeunggiJung@users.noreply.github.com>
2026-04-19 16:06:32 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 404 ms, Memory: 25024 KB -BaekjoonHub

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.cc
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.cc
@@ -1,71 +1,13 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <map>
-#include <climits>
-
-using namespace std;
-typedef pair<int, int> pii;
-// 위상 정렬 다시 풀기 (indegree 이용)
-int N, M;
-vector<int> indegree;
-vector<vector<string>> childs;
-vector<string> toname;
-map<string, int> tonum;
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    cin>>N;
-    indegree.assign(N, 0);
-    childs.assign(N, {});
-    for(int i=0;i<N;i++){
-        string name;
-        cin>>name;
-        tonum[name] = i;
-        toname.push_back(name);
-    }
-    cin>>M;
-    for(int i=0;i<M;i++){
-        string c, p;
-        cin>>c>>p;
-        childs[tonum[p]].push_back(c);
-        indegree[tonum[c]]++;
-    }
-    priority_queue<pair<string, vector<string>>, vector<pair<string, vector<string>>>, greater<pair<string, vector<string>>>> pq;
-    queue<int> q;
-    for(int i=0;i<N;i++){
-        if(!indegree[i]) q.push(i);
-    }
-    int qs = q.size();
-    cout<<qs<<"\n";
-    int cnt = 0;
-    while(!q.empty()){
-        int cur = q.front();
-        if(cnt<qs){
-            cout<<toname[cur]<<" ";
-            cnt++;
-        }
-        q.pop();
-        pair<string, vector<string>> a = {toname[cur], {}};
-        for(auto& x : childs[cur]){
-            indegree[tonum[x]]--;
-            if(!indegree[tonum[x]]){
-                a.second.push_back(x);
-                q.push(tonum[x]);
-            }
-        }
-        pq.push(a);
-    }
-    cout<<"\n";
-    while(!pq.empty()){
-        cout<<pq.top().first<<" ";
-        cout<<pq.top().second.size()<<" ";
-        for(auto& x : pq.top().second){
-            cout<<x<<" ";
-        }
-        cout<<"\n";
-        pq.pop();
-    }
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+<hr><center>nginx/1.22.1</center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
